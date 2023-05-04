2d8c229a5fbb9f61865fe9818dcdc125605b98d1
camileonzapata <camileonzapata@gmail.com>
2023-05-04 08:21:39 -0600

    se guardaron los cambios

--- unidad-4/practica-2/practica.sh
+++ unidad-4/practica-2/practica.sh
@@ -1,2 +1,44 @@
-chmod +x unidad-4/practica-2/practica.sh
+#!/bin/bash
+
+
+echo "Seleccione una opción:"
+echo "1. Comprimir con el comando zip"
+echo "2. Comprimir con el comando rar"
+read opcion
+
+
+echo "Ingrese el nombre de la carpeta a comprimir:"
+read carpeta
+echo "Ingrese el nombre del archivo comprimido:"
+read archivo
+
+
+mkdir $carpeta
+touch $carpeta/index.html
+
+
+if [ $opcion -eq 1 ]
+then
+    zip $archivo $carpeta
+else
+    rar a $archivo $carpeta
+fi
+
+
+echo "¿Desea descomprimir el archivo? (SI/NO)"
+read respuesta
+
+
+if [ $respuesta = "SI" ]
+then
+    mkdir practica-2/descomprimidos
+    if [ $opcion -eq 1 ]
+    then
+        unzip $archivo -d practica-2/descomprimidos
+    else
+        unrar x $archivo practica-2/descomprimidos
+    fi
+fi
+
+
 
